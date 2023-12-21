3803247caa38adb74b3fcc0a00fc6b331cc64c05
Ethan Bober <boberethan@gmail.com>
2023-12-20 22:52:20 -0500

    Added typing effect

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// JavaScript code goes here
+// JavaScript code for dynamic typing effect
 
-function myFunction() {
-    document.querySelector("#replace").innerHTML = "Hello World!";
-  }
+function typeWriter(text, elementId, speed) {
+    let i = 0;
+    function type() {
+        if (i < text.length) {
+            document.getElementById(elementId).innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    typeWriter("👋 Hi there, ", "line1", 50); // Speed can be adjusted
+    setTimeout(() => typeWriter("I'm Ethan Bober.", "line2", 50), 700); // Start after a delay
+});
