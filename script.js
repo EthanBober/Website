bb5586ddf84e5f6e5c98b10e44edcc5ae2c7ab81
Ethan Bober <boberethan@gmail.com>
2024-09-18 22:10:06 -0400

    Added typing nodal struc

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 const consoleElement = document.getElementById('console');
 
-function printToConsoleNoHead(text) {
+function typeHtml(element, htmlString, delay, callback) {
+    const tempContainer = document.createElement('div');
+    tempContainer.innerHTML = htmlString;
+
+    function processNode(node, parentElement, done) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            typeTextNode(node, parentElement, delay, done);
+        } else if (node.nodeType === Node.ELEMENT_NODE) {
+            const clonedNode = node.cloneNode(false);
+            parentElement.appendChild(clonedNode);
+
+            const childNodes = Array.from(node.childNodes);
+            processNodesSequentially(childNodes, clonedNode, done);
+        }
+    }
+
+    function processNodesSequentially(nodes, parentElement, done) {
+        if (nodes.length === 0) {
+            done();
+            return;
+        }
+
+        const [firstNode, ...restNodes] = nodes;
+        processNode(firstNode, parentElement, function() {
+            processNodesSequentially(restNodes, parentElement, done);
+        });
+    }
+
+    function typeTextNode(textNode, parentElement, delay, done) {
+        const textContent = textNode.textContent;
+        let i = 0;
+        function typeNextChar() {
+            if (i < textContent.length) {
+                parentElement.appendChild(document.createTextNode(textContent.charAt(i)));
+                i++;
+                setTimeout(typeNextChar, delay);
+            } else {
+                done();
+            }
+        }
+        typeNextChar();
+    }
+
+    const childNodes = Array.from(tempContainer.childNodes);
+    processNodesSequentially(childNodes, element, function() {
+        if (callback) callback();
+    });
+}
+
+function printToConsoleNoHead(text, callback) {
     const outputContainer = document.createElement('div');
     outputContainer.className = 'output-container';
 
@@ -8,21 +57,16 @@ function printToConsoleNoHead(text) {
     promptSymbol.className = 'prompt-symbol';
 
     const outputText = document.createElement('span');
-    
-    outputText.textContent = text;
     outputText.className = 'output-text';
-
-
 
     outputContainer.appendChild(promptSymbol);
     outputContainer.appendChild(outputText);
     consoleElement.appendChild(outputContainer);
+
+    typeHtml(outputText, text, 10, callback);
 }
 
-
-
-
-function printToConsole(text) {
+function printToConsole(text, callback) {
     const outputContainer = document.createElement('div');
     outputContainer.className = 'output-container';
 
@@ -31,16 +75,14 @@ function printToConsole(text) {
     promptSymbol.className = 'prompt-symbol';
 
     const outputText = document.createElement('span');
-
-    outputText.innerHTML = text;
     outputText.className = 'output-text';
 
     outputContainer.appendChild(promptSymbol);
     outputContainer.appendChild(outputText);
     consoleElement.appendChild(outputContainer);
+
+    typeHtml(outputText, text, 10, callback);
 }
-
-
 
 function promptInput() {
     const inputContainer = document.createElement('div');
@@ -64,10 +106,8 @@ function promptInput() {
         if (event.key === 'Enter') {
             const command = input.value.trim();
 
-            // Disable the input field
             input.disabled = true;
 
-            // Replace the input field with a span
             const commandText = document.createElement('span');
             commandText.textContent = input.value;
             commandText.className = 'command-text';
@@ -82,35 +122,39 @@ function promptInput() {
 function handleCommand(command) {
     switch (command.toLowerCase()) {
         case 'help':
-            printToConsole('Available commands:\n- help\n- aboutme\n- resume \n- thingsido \n- socials\n- clear');
+            printToConsole('Available commands:\n- help\n- aboutme\n- resume \n- thingsido \n- socials\n- clear', promptInput);
             break;
         case 'aboutme':
-            printToConsole('Hey! 👋 I\'m Ethan. \n I\'m currently studying Material Science and Engineering at the University of Pennsylvania \n I do a bit of research on campus and am involved with a few engineering related clubs. \n My main interests are electronic/nanomaterials. \n Feel free to reach me at ebober@seas.upenn.edu');
+            printToConsole('Hey! 👋 I\'m Ethan. \nI\'m currently studying Material Science and Engineering at the University of Pennsylvania.\nI do a bit of research on campus and am involved with a few engineering-related clubs.\nMy main interests are electronic/nanomaterials.\nFeel free to reach me at ebober@seas.upenn.edu', promptInput);
             break;
         case 'resume':
-            printToConsole('Redirecting to resume...');
-            window.open('resumeUp.pdf', '_blank');
+            printToConsole('Redirecting to resume...', function() {
+                window.open('resumeUp.pdf', '_blank');
+                promptInput();
+            });
             break;
         case 'socials':
-            printToConsole('Connect with me on: <a href="https://www.linkedin.com/in/ethanbober/" target="_blank">LinkedIn</a> | <a href="https://github.com/EthanBober" target="_blank">GitHub</a>');
+            printToConsole('Connect with me on: <a href="https://www.linkedin.com/in/ethanbober/" target="_blank">LinkedIn</a> | <a href="https://github.com/EthanBober" target="_blank">GitHub</a>', promptInput);
             break;
         case 'maxx':
-            printToConsole('secret found...');
-            window.open('https://www.maxxyung.com/', '_blank');
+            printToConsole('secret found...', function() {
+                window.open('https://www.maxxyung.com/', '_blank');
+                promptInput();
+            });
             break;
         case 'thingsido':
-            printToConsole('Right now, I am currently involved with research with the Shu Yang Group on <span style="color:blue;">hydroscopic hydrogel desiccants</span>.  \n Mainly, I synthesize, analyze, and compare performances of the different monomers used for the synthesis. \n Other than that, I also help volunteer for engineering education as a part of Access Engineering and also volunteer in the SOUP comp. here at the UofPenn. \n For a full list, check out my <span style="color:yellow;">resume</span> with the "resume" command.')
+            printToConsole('Right now, I am currently involved with research with the Shu Yang Group on <span style="color:blue;">hydroscopic hydrogel desiccants</span>.\nMainly, I synthesize, analyze, and compare performances of the different monomers used for the synthesis.\nOther than that, I also help volunteer for engineering education as a part of Access Engineering and also volunteer in the SOUP comp. here at the UofPenn.\nFor a full list, check out my <span style="color:yellow;">resume</span> with the "resume" command.', promptInput);
             break;
         case 'clear':
             consoleElement.innerHTML = '';
+            promptInput();
             break;
         default:
-            printToConsole(`Command not recognized: "${command}". Type "help" for a list of commands.`);
+            printToConsole(`Command not recognized: "${command}". Type "help" for a list of commands.`, promptInput);
     }
-    promptInput();
 }
 
-// Initialize the console
-printToConsoleNoHead("____________________                   ________      ______ \n___  ____/_  /___  /_______ _______    ___  __ )________  /______________\n__  __/  _  __/_  __ \  __ `/_  __ \     __  __  |  __ \_  __ \  _ \_  ___/\n_  /___  / /_ _  / / / /_/ /_  / / /   _  /_/ // /_/ /  /_/ /  __/  /    \n/_____/  \__/ /_/ /_/\__,_/ /_/ /_/      /_____/ \____//_.___/\___//_/")
-printToConsole('Welcome to my portfolio! Type "help" for a list of commands.');
-promptInput();
+
+printToConsoleNoHead("____________________                   ________      ______ \n___  ____/_  /___  /_______ _______    ___  __ )________  /______________\n__  __/  _  __/_  __ \  __ `/_  __ \     __  __  |  __ \_  __ \  _ \_  ___/\n_  /___  / /_ _  / / / /_/ /_  / / /   _  /_/ // /_/ /  /_/ /  __/  /    \n/_____/  \__/ /_/ /_/\__,_/ /_/ /_/      /_____/ \____//_.___/\___//_/", function() {
+    printToConsole('Welcome to my portfolio! Type "help" for a list of commands.', promptInput);
+});
